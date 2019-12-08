d159229a2fbcb8295dd1e004e77b39cf419ce278
Clinton <c.bestmann@hotmail.com>
2019-12-07 19:46:14 -0800

    added font-awesom and created header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './header.styles.scss';
+
+const Header = () => (
+    <header className="header">
+        <h3 className="title">Where in the world?</h3>
+        <span className="theme-switch">Dark Mode</span>
+    </header>
+);
+
+export default Header;
